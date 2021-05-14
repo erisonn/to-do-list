235d1e602356e87376043c7c3156360ab9ebc5dc
Erison <erisonn.santos@gmail.com>
2021-05-14 17:32:59 -0300

    implementando API localStorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,39 +1,29 @@
 // carrega as tarefas salvas em localStorage ao carregar a pagina;
 carregarTarefas = () => {
   var tarefasArray = JSON.parse(localStorage.getItem('tarefas'));
+  var tarefasConcluidasArray = JSON.parse(localStorage.getItem('tarefas concluidas'));
   Array.from(tarefasArray).forEach(function(element) {
     var item = document.createElement('li');
     item.textContent = element;
     item.classList.add('tarefa-item');
+    if(tarefasConcluidasArray.includes(item.textContent) === true) {
+        item.classList.add('tarefa-feita');
+    }
     tarefas_lista.append(item);
-    var tarefa_item = document.querySelectorAll('.tarefa-item');
-    var apagar_tarefa = document.createElement('span');
-    apagar_tarefa.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    apagar_tarefa.classList.add('apagar-tarefa');
-    Array.from(tarefa_item).forEach(function(item) {
-        item.append(apagar_tarefa);
-        })
-    })
-}
-// adiciona os eventlisteners as tarefas carregadas;
-salvarTarefas = () => {
-    var tarefa_item = document.querySelectorAll('.tarefa-item');
-    Array.from(tarefa_item).forEach(function(item) {
-        tarefasArray.push(item.textContent);
-        let del_tarefa = document.querySelectorAll('.apagar-tarefa');
-        Array.from(del_tarefa).forEach(function(element) {
-        element.addEventListener('click', function() {
-            var tarefa_removida = element.parentElement.textContent;
-                for(i in tarefasArray) {
-                    if(tarefasArray[i] === tarefa_removida) {
-                        tarefasArray.splice(i, 1);
-                        localStorage.setItem('tarefas', JSON.stringify(tarefasArray));
-                    }
-                }
-            element.parentElement.remove();
-            })
-        })
+    lixeiraIcone();
     })
 }
 
-window.onload = carregarTarefas(); salvarTarefas(); nova_tarefa.value = ''
+salvarTarefas = () => {
+    var tarefa_item = document.querySelectorAll('.tarefa-item');
+    var tarefa_feita = document.querySelectorAll('.tarefa-feita');
+    Array.from(tarefa_feita).forEach(function(item) {
+        tarefasConcluidasArray.push(item.textContent);
+    })
+    Array.from(tarefa_item).forEach(function(item) {
+        tarefasArray.push(item.textContent);
+        delTask();
+    })  
+}
+
+window.onload = carregarTarefas(); salvarTarefas(); completeTask(); nova_tarefa.value = ''
--- js/script.js
+++ js/script.js
@@ -4,56 +4,70 @@ const tarefas_lista = document.querySelector('.tarefas-lista');
 var tarefasArray = [];
 var tarefasConcluidasArray = [];
 
-newTask = () => {
-    if (nova_tarefa.value == '') {
-        window.alert('Digite uma tarefa!')
-    } else {
-// adiciona tarefa para a lista;
-        var item = document.createElement('li');
-        item.textContent = nova_tarefa.value;
-        tarefasArray.push(item.textContent);
-        localStorage.setItem('tarefas', JSON.stringify(tarefasArray)); // salva a tarefa adicionada em LocalStorage;
-        item.classList.add('tarefa-item');
-        tarefas_lista.append(item);
-        nova_tarefa.value = ''
-        var tarefa_item = document.querySelectorAll('.tarefa-item');
-    
-// criar botão de excluir tarefa;
-        var apagar_tarefa = document.createElement('span');
-        apagar_tarefa.innerHTML = '<i class="fas fa-trash-alt"></i>';
-        apagar_tarefa.classList.add('apagar-tarefa');
-        for(i in tarefa_item) {
-        tarefa_item[i].append(apagar_tarefa);
-
-// exclui item da lista ao clicar no icone da lixeira;
-        let del_tarefa = document.querySelectorAll('.apagar-tarefa');
-        Array.from(del_tarefa).forEach(function(element) {
-            element.addEventListener('click', function() {
-                var tarefa_removida = element.parentElement.textContent;
-                for(i in tarefasArray) {
-                    if(tarefasArray[i] === tarefa_removida) {
-                        tarefasArray.splice(i, 1);                                      // remove a tarefa de LocalStorage;
-                        localStorage.setItem('tarefas', JSON.stringify(tarefasArray));
-                    }
-                }
-                element.parentElement.remove();
-                })
-            })
-        }
-        nova_tarefa.value = ""
-    }
-}
-
 add_tarefa.addEventListener('click', () => {
     newTask();
 })
 
-// conclui tarefa ao clicar;
+newTask = () => {
+    if (nova_tarefa.value == '') {
+        window.alert('Digite uma tarefa!')
+    } else {
+        var item = document.createElement('li');
+        item.textContent = nova_tarefa.value;
+        tarefasArray.push(item.textContent);
+        localStorage.setItem('tarefas', JSON.stringify(tarefasArray));
+        item.classList.add('tarefa-item');
+        tarefas_lista.append(item);
+        nova_tarefa.value = ''
+        lixeiraIcone();
+        delTask();
+    }
+}
 
-var tarefas_adicionadas = document.querySelector('ul');
-tarefas_adicionadas.addEventListener('click', function (click) {
+lixeiraIcone = () => {
+    var tarefa_item = document.querySelectorAll('.tarefa-item');
+    var apagar_tarefa = document.createElement('span');
+    apagar_tarefa.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    apagar_tarefa.classList.add('apagar-tarefa');
+    Array.from(tarefa_item).forEach(function(item) {
+        item.append(apagar_tarefa);
+    })
+}
+
+delTask = () => {
+    let del_tarefa = document.querySelectorAll('.apagar-tarefa');
+    Array.from(del_tarefa).forEach(function(element) {
+        element.addEventListener('click', function() {
+            var tarefa_removida = element.parentElement.textContent;
+            for(i in tarefasArray) {
+                 if(tarefasArray[i] === tarefa_removida) {
+                    tarefasArray.splice(i, 1);
+                    localStorage.setItem('tarefas', JSON.stringify(tarefasArray));
+                 }
+             }
+            for(i in tarefasConcluidasArray) {
+                if(tarefasConcluidasArray[i] === tarefa_removida) {
+                    tarefasConcluidasArray.splice(i, 1);
+                    localStorage.setItem('tarefas concluidas', JSON.stringify(tarefasConcluidasArray));
+                }
+            }
+            element.parentElement.remove();
+        })
+    })
+}
+
+// conclui tarefa ao clicar;
+completeTask = () => {
+    var tarefas_adicionadas = document.querySelector('ul');
+    tarefas_adicionadas.addEventListener('click', function (click) {
     if(click.target.tagName === 'LI') {
         click.target.classList.toggle('tarefa-feita');
-    }
-})
-
+        var tarefa_feita = click.target.textContent;
+        if(tarefasConcluidasArray.includes(tarefa_feita) === false) {
+            tarefasConcluidasArray.push(tarefa_feita);
+            localStorage.setItem('tarefas concluidas', JSON.stringify(tarefasConcluidasArray));
+            console.log(tarefasConcluidasArray);
+            }
+        }
+    })
+}
